364388087f44f9581b0888fe1c23d3f3e1ddad2f
internfomrahousing <intern.fomrahousing@gmail.com>
2026-06-25 12:46:09 +0530

    Always show GPS Patta button; detect location inline if not set

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"77e2e94772b6eb43759e34ed1ad7da4674e19c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1119769329" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "322435944" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
